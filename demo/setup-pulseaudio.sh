e9169df81b14ba9682d6ecb2f5546d7d0f50898c
vintitres <vintitres@users.noreply.github.com>
2025-04-24 02:48:29 +0000

    builds; kinda runs

--- demo/setup-pulseaudio.sh
+++ demo/setup-pulseaudio.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # enable dbus
 
-mkdir -p /var/run/dbus
-dbus-uuidgen > /var/lib/dbus/machine-id
-dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
+sudo mkdir -p /var/run/dbus
+sudo dbus-uuidgen > /var/lib/dbus/machine-id
+sudo dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
 
 
 # add root
-adduser root pulse-access
-adduser root audio
+sudo adduser root pulse-access
+sudo adduser root audio
 
 # Cleanup to be "stateless" on startup, otherwise pulseaudio daemon can't start
 
-rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
+sudo rm -rf /var/run/pulse /var/lib/pulse /root/.config/pulse
 cp /etc/pulse/* ~/.config/pulse/
 
 pulseaudio -D --exit-idle-time=-1
